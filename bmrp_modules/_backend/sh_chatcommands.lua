d23f0b45bfdbb7ac8c68e847d429d2923bc1a12a
Solomon <robindoger@gmail.com>
2019-06-18 00:09:04 -0400

    70% of the way done with BMRP Commands

--- bmrp_modules/_backend/sh_chatcommands.lua
+++ bmrp_modules/_backend/sh_chatcommands.lua
@@ -59,6 +59,175 @@ voxtime:addParam{type=ULib.cmds.NumArg, min=10, max=720,default=60,hint="time(se
 voxtime:defaultAccess(ULib.ACCESS_ADMIN)
 voxtime:help("Sets the vox time in seconds.")
 
+--[[-------------------------------------------------------------------------
+Advert/Announce
+---------------------------------------------------------------------------]]
+sBMRP.AnnounceState = true
+local function PlayerAnnounce(ply, args)
+    if args == "" then
+        DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", "argument", ""))
+        return ""
+    end
+	
+	local Team = ply:Team()
+	if ( Team == TEAM_VISITOR or  Team == TEAM_TESTSUBJECT or  Team == TEAM_HEADCRAB ) then
+        DarkRP.notify(ply, 1, 4, "You do not have a radio")
+        return ""
+    end
+	
+	if !sBMRP.AnnounceState then
+		ply:ChatPrint("You try to send the message, but all you can hear is static on the other end. You assume no one heard it.")
+        return ""
+	end
+	if ply:IsAlien() then
+		DoSay = function(text)
+			if text == "" then
+				DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", "argument", ""))
+				return
+			end
+			for k,v in pairs(player.GetAll()) do
+				local col = team.GetColor(ply:Team())
+				if CheckInRange(Vector(-11615.540039,-4814.913574,-5864.372070),Vector(-1696.177734,-13936.375977,2038.580933),v:GetPos()) then 
+					DarkRP.talkToPerson(v, col, "[Xenian Communication] " .. ply:Nick(), Color(255, 255, 0, 255), text, ply)
+				end
+			end
+		end
+	else
+		DoSay = function(text)
+			if text == "" then
+				DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", "argument", ""))
+				return
+			end
+			for k,v in pairs(player.GetAll()) do
+				local col = team.GetColor(ply:Team())
+				DarkRP.talkToPerson(v, col, "[Announcement] " .. ply:Nick(), Color(255, 255, 0, 255), text, ply)
+			end
+		end
+	end
+    return args, DoSay
+end
+if SERVER then
+	sBMRP.CreateChatCommand("announce", PlayerAnnounce, "Announce your message.", 10)
+	sBMRP.CreateChatCommand("advert", PlayerAnnounce, "Announce your message.", 10)
+end
+--[[-------------------------------------------------------------------------
+Disable Announce/Advert
+---------------------------------------------------------------------------]]
+function ulx.toggleannounce(calling_ply, args)
+
+	if sBMRP.AnnounceState then
+	    sBMRP.AnnounceState = false
+		ulx.fancyLog( player.GetAdmins(), "#P disabled the announcement system.",calling_ply)
+	else
+	    sBMRP.AnnounceState = true
+		ulx.fancyLog( player.GetAdmins(), "#P enabled the announcement system.",calling_ply)
+	end
+end
+local toggleannounce = ulx.command(CATEGORY_NAME, "ulx toggleannounce", ulx.toggleannounce, "!toggleannounce", true, false )
+toggleannounce:defaultAccess(ULib.ACCESS_ADMIN)
+toggleannounce:help("Disable or enable the announcement system.")
+
+--[[-------------------------------------------------------------------------
+Building toggle
+---------------------------------------------------------------------------]]
+sBMRP.DisableBuilding = false
+function ulx.DisableBuilding(calling_ply)	
+	if not sBMRP.DisableBuilding then
+	    sBMRP.DisableBuilding = true
+		ulx.fancyLog( player.GetAll(), "#P disabled building for players.",calling_ply)
+		local function blockspawning(ply)
+			if ply:IsAdmin() then
+				return false
+			end
+		end
+		for k,v in pairs({"PlayerSpawnProp", "CanTool", "PlayerSpawnVehicle","PlayerSpawnRagdoll", "PlayerSpawnEffect"}) do
+			hook.Add(v, "sBMRP.BlockSpawning", blockspawning)
+		end
+
+	else
+	    sBMRP.DisableBuilding = false
+		ulx.fancyLog( player.GetAll(), "#P enabled building for players.",calling_ply)
+		for k,v in pairs({"PlayerSpawnProp", "CanTool", "PlayerSpawnVehicle","PlayerSpawnRagdoll", "PlayerSpawnEffect"}) do
+			hook.Remove(v, "sBMRP.BlockSpawning")
+		end
+	end
+end
+local DisableBuilding = ulx.command(CATEGORY_NAME, "ulx disablebuilding", ulx.DisableBuilding, "!disablebuilding", true, false)
+DisableBuilding:defaultAccess(ULib.ACCESS_ADMIN)
+DisableBuilding:help("Enable or disable propspawing and toolgun usage.")
+
+--[[-------------------------------------------------------------------------
+Xenian Toggle
+---------------------------------------------------------------------------]]
+
+function ulx.allowsinglexenian(calling_ply, target_ply, disallow)
+	local target = target_ply[1]
+	local ply = calling_ply
+	if !target:IsAlien() then
+		ULib.tsayError( calling_ply, "The player targeted is not a xenian.", true )
+		return
+	end
+	local recepients = table.insert(player.GetAdmins(), target)
+	if not disallow then
+		ply:AllowToEarth(true)
+		ulx.fancyLog( {unpack(player.GetAdmins()), target}, "#P gave #P permission go to earth.",calling_ply, target)
+	else
+		ulx.fancyLog({unpack(player.GetAdmins()), target}, "#P revoked #P's permission to go earth.",calling_ply, target)
+		ply:AllowToEarth(false)
+	end
+
+	local function resetperms(ply)
+		ply:AllowToEarth(false)
+		for k,v in pairs("PlayerDeath", "OnPlayerChangedTeam") do
+			hook.Remove(k, "sBMRP.EarthRestrictPerms")
+		end
+	end
+	for k,v in pairs("PlayerDeath", "OnPlayerChangedTeam") do
+		hook.Add(k, "sBMRP.EarthRestrictPerms", resetperms)
+	end
+end
+local allowsinglexenian = ulx.command(CATEGORY_NAME, "ulx allowsinglexenian", ulx.allowsinglexenian, "!allowsinglexenian", true, false)
+allowsinglexenian:addParam{ type=ULib.cmds.PlayersArg }
+allowsinglexenian:addParam{ type=ULib.cmds.BoolArg, invisible=true}
+allowsinglexenian:defaultAccess(ULib.ACCESS_ADMIN)
+allowsinglexenian:help("Allow the temporary access of a xenian player, will auto revoke on death/teamchange.")
+allowsinglexenian:setOpposite( "ulx unallowsinglexenian", {_, _, true}, "!unallowsinglexenian" )
+
+function ulx.allowsinglehecu(calling_ply, target_ply, disallow)
+	local target = target_ply[1]
+	local ply = calling_ply
+	local reciptients = {}
+
+	if !target:IsHECU() then
+		ULib.tsayError( calling_ply, "The player targeted is not a HECU.", true )
+		return
+	end
+	if not disallow then
+		ply:AllowToBMRF(true)
+		ulx.fancyLog( {unpack(player.GetAdmins()), target}, "#P gave #P permission go to earth.",calling_ply, target)
+	else
+		ulx.fancyLog( {unpack(player.GetAdmins()), target}, "#P revoked #P's permission to go earth.",calling_ply, target)
+		ply:AllowToBMRF(false)
+	end
+
+	local function resetperms(ply)
+		ply:AllowToBMRF(false)
+		for k,v in pairs("PlayerDeath", "OnPlayerChangedTeam") do
+			hook.Remove(k, "sBMRP.AllowToBMRF")
+		end
+	end
+	for k,v in pairs("PlayerDeath", "OnPlayerChangedTeam") do
+		hook.Add(k, "sBMRP.AllowToBMRF", resetperms)
+	end
+end
+local allowsinglehecu = ulx.command(CATEGORY_NAME, "ulx allowsinglehecu", ulx.allowsinglehecu, "!allowsinglehecu", true, false)
+allowsinglehecu:addParam{ type=ULib.cmds.PlayersArg }
+allowsinglehecu:addParam{ type=ULib.cmds.BoolArg, invisible=true}
+allowsinglehecu:defaultAccess(ULib.ACCESS_ADMIN)
+allowsinglehecu:help("Allow the temporary access of a HECU player, will auto revoke on death/teamchange.")
+allowsinglehecu:setOpposite( "ulx unallowsinglehecu", {_, _, true}, "!unallowsinglehecu" )
+
+
 
 
 --[[-------------------------------------------------------------------------
