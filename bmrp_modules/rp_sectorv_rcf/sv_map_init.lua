c11f9515c2fa83d4171ce6cb90590ad57f3bebdc
Solomon <robindoger@gmail.com>
2019-06-27 22:59:03 -0400

    Map Initiation

--- bmrp_modules/rp_sectorv_rcf/sv_map_init.lua
+++ bmrp_modules/rp_sectorv_rcf/sv_map_init.lua
@@ -2,4 +2,17 @@
 Map init/cleanup handler
 ---------------------------------------------------------------------------]]
 
-local function a() return end
+local function mapinit()
+
+	local xenelevator = ents.GetMapCreatedEntity(4781)
+
+	xenelevator:SetKeyValue("noise1","plats/elevator_start1.wav")
+	xenelevator:SetKeyValue("noise2","plats/talkmove2.wav")
+	xenelevator:SetKeyValue("startclosesound", Sound("plats/railstop1.wav"))
+
+end
+
+
+
+
+sBMRP.MapHook("map_init", mapinit)
