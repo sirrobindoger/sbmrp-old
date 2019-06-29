0dc41ddb1f4078b790ec4e44356c596f818a748b
Solomon <robindoger@gmail.com>
2019-06-28 23:13:25 -0400

    Ambient Sound Fix

--- bmrp_modules/rp_sectorv_rcf/sv_map_init.lua
+++ bmrp_modules/rp_sectorv_rcf/sv_map_init.lua
@@ -4,15 +4,36 @@ Map init/cleanup handler
 
 local function mapinit()
 
-	local xenelevator = ents.GetMapCreatedEntity(4781)
+	--[[-------------------------------------------------------------------------
+	Sounds
+	---------------------------------------------------------------------------]]
 
-	xenelevator:SetKeyValue("noise1","plats/elevator_start1.wav")
-	xenelevator:SetKeyValue("noise2","plats/talkmove2.wav")
-	xenelevator:SetKeyValue("startclosesound", Sound("plats/railstop1.wav"))
-
+	for k,v in pairs({5201, 5198}) do
+		SafeRemoveEntity(ents.GetMapCreatedEntity(v))
+	end
+	timer.Create("ambience-sound_fix", 1, 0, function()
+		for k,v in pairs(ents.FindByName("amb")) do
+			local shouldplay = false
+			for k,ply in pairs(ents.FindInSphere(v:GetPos(),20)) do
+				if ply:IsPlayer() then
+					shouldplay = true
+					break
+				end
+			end
+			if not shouldplay then
+				v:Fire("StopSound")
+			else
+				v:Fire("PlaySound")
+			end
+		end
+	end)
 end
-
-
+/*
+for k,v in pairs(ents.GetAll()) do
+	if v:GetPos():WithinAABox(Vector(6196.2192382813,1101.5805664063,173.81211853027),Vector(6443.576171875,814.38983154297,-30.359924316406)) then
+		print(v:GetClass() .. " " .. v:MapCreationID())
+	end
+end*/
 
 
 sBMRP.MapHook("map_init", mapinit)
