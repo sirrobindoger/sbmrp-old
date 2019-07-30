a398948845743854940d25d8bcab078398bd0aed
Solomon <robindoger@gmail.com>
2019-07-30 14:16:55 -0400

    Sum more job functions hek yea

--- bmrp_modules/roleplay-manager/sh_job-functions.lua
+++ bmrp_modules/roleplay-manager/sh_job-functions.lua
@@ -93,6 +93,40 @@ function ply:HasHEV()
 end
 
 --[[-------------------------------------------------------------------------
+Retreive job functions
+---------------------------------------------------------------------------]]
+
+function player.GetHECU()
+	local ply = {}
+	for k,v in pairs(player.GetAll()) do
+		if v:IsHECU() then
+			table.insert(ply, v)
+		end
+	end
+	return ply
+end
+
+function player.GetAlien()
+	local ply = {}
+	for k,v in pairs(player.GetAll()) do
+		if v:IsAlien() then
+			table.insert(ply, v)
+		end
+	end
+	return ply
+end
+
+function player.GetSecurity()
+	local ply = {}
+	for k,v in pairs(player.GetAll()) do
+		if v:IsSecurity() then
+			table.insert(ply, v)
+		end
+	end
+	return ply
+end
+
+--[[-------------------------------------------------------------------------
 Functions to check if x player is allowed at x location
 ---------------------------------------------------------------------------]]
 function ply:IsAllowedXen()
@@ -320,74 +354,6 @@ function Taze(ent,mode)
 		end)
 	end
 end
-
---[[-------------------------------------------------------------------------
-HECU Code
----------------------------------------------------------------------------]]
-
-if CLIENT then
-	local codefont = sBMRP.AppendFont("codefont", ScreenScale(10))
-	local HECU_Codes = {
-		["Red"] = {
-			color = {255,0,0, 156},
-			text = "Evacuate Facility"
-		},
-		["Green"] = {
-			color = {0,150,0, 156},
-			text = "No Active Threats",
-		},
-		["Black"] = {
-			color = {0,0,0, 156},
-			text = "Kill On Sight",
-		},
-		["Yellow"] = {
-			color = {212, 184, 28, 156},
-			text = "On Alert"
-		},
-	}
-	hook.Add("HUDPaint", "hecu-code_hud", function()
-		local ply = LocalPlayer()
-		if not ply:IsSirro() then return end
-		surface.SetFont(codefont)
-		local codename = GetGlobalString("HECUCode", "Green")
-		local titlename = "HECU Code: "
-
-		local titlesize = surface.GetTextSize(titlename)
-		local textsize = surface.GetTextSize(codename)
-		local subtextsize = surface.GetTextSize(HECU_Codes[codename].text)
-		local boxlength = 190
-
-		local titlepos = 533 + math.abs(boxlength-titlesize)/2
-		local codepos = 553 + math.abs(boxlength-textsize)/2
-		local subpos = 533 + math.abs(boxlength-subtextsize)
-		
-		-- Box
-		surface.SetDrawColor( 0, 0, 0, 255 )
-		surface.DrawRect( ScrW() *(553/1280), ScrH()*(0/720), ScrW() *(boxlength/1280),ScrH() *(40/720))
-		-- Header
-		surface.SetDrawColor( 50,50,50, 255 )
-		surface.DrawRect( ScrW() *(553/1280), ScrH()*(0/720),ScrW() *(boxlength/1280),ScrH()*(20/720) )
-		-- Color box
-		surface.SetDrawColor( unpack(HECU_Codes[codename].color))
-		surface.DrawRect( ScrW() *(553/1280), ScrH()*(20/720),ScrW() *((boxlength)/1280), ScrH()*(20/720) )
-		-- Title
-		surface.SetTextColor(255,255,255,100)
-		surface.SetTextPos(ScrW() *(titlepos/1280),0)
-		surface.DrawText(titlename)
-		-- Title 1
-		surface.SetTextColor(unpack(HECU_Codes[codename].color))
-		surface.SetTextPos(ScrW() *(titlepos*1.16 /1280),0)
-		surface.DrawText(codename)
-
-		-- subtext 1
-		surface.SetTextColor(255,255,255)
-		surface.SetTextPos(ScrW() *(subpos/1280),ScrH()*(20/720))
-		surface.DrawText(HECU_Codes[codename].text)
-	end)
-end
-
-
-
 
 
 --[[-------------------------------------------------------------------------
