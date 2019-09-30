a7af9dbe6e0f3cfff6998f8669a439bfc6af17a1
Solomon <robindoger@gmail.com>
2019-09-30 13:07:54 -0400

    Added TOOL support to hot mounter.

--- bmrp_modules/_backend/sh_net_compress_table.lua
+++ bmrp_modules/_backend/sh_net_compress_table.lua
@@ -45,11 +45,12 @@ Shared section
 ---------------------------------------------------------------------------]]
 local oPrint = oPrint || print
 local print = function(...) return oPrint("[sHotMount] " .. ...) end -- Yes, I'm detouring print cause i'm that lazy, fuck you.
-function compileWeapon(path, wepname)
+function compileWeapon(path, wepname, tool)
+	local wepname = wepname:Replace(".lua", "")
+	--if tool then print(wepname .. " is a tool, those are not supported (yet), skipping.") return end
 	oldCSLua = oldCSLua || AddCSLuaFile
 	oldInclude = oldInclude || include
 	AddCSLuaFile = function()  end
-	file.Write("hehe.txt", file.Read(path, "LUA"))
 	include = function(p)
 		if #p:Split("/") <= 1 then
 			if SERVER then
@@ -61,13 +62,47 @@ function compileWeapon(path, wepname)
 		return CompileFile(p)() 
 	end
 	SWEP = weapons.Get("weapon_base")
+	--[[
+		Tool object registry
+	]]
+	if tool then
+		ToolObj = newToolObj()
+		TOOL = ToolObj:Create()
+		TOOL.Mode = wepname
+		
+	end
 	if SERVER then
 		CompileFile(path)()
 	else
 		CompileString(f_Read(path), path)()
 	end
+	--[[
+		Add tool into menu system.
+	]]
+	if tool then
+		toolName = TOOL.Name or "#" .. wepname
+		TOOL:CreateConVars()
+		//SWEP.Tool[ wepname ] = TOOL
+		hook.Add( "PopulateToolMenu", "sHotMount_tool_" .. wepname, function()
+			if ( TOOL.AddToMenu != false ) then
+				spawnmenu.AddToolMenuOption(
+					TOOL.Tab or "Main",
+					TOOL.Category or "New Category",
+					wepname,
+					TOOL.Name or "#" .. wepname,
+					TOOL.Command or "gmod_tool " .. wepname,
+					TOOL.ConfigName or wepname,
+					TOOL.BuildCPanel
+				)
+			end
+
+		end)
+		TOOL = nil
+		ToolObj = nil
+	end
+
 	weapons.Register(SWEP, wepname:Replace(".lua", ""))
-	print("Registered weapon: " .. SWEP.PrintName || "NULL")
+	print("Registered " .. (tool and "tool: " .. toolName or "weapon: " .. SWEP.PrintName ) || "NULL")
 	SWEP = nil
 	AddCSLuaFile = oldCSLua
 	include = oldInclude
@@ -78,7 +113,6 @@ function compileEnt(path, enttname)
 	oldInclude = oldInclude || include
 	AddCSLuaFile = function()  end
 	include = function(p)
-		print(#path:Split("/") .. "<-- PATH")
 		if #p:Split("/") <= 1 then
 			if SERVER then
 				return CompileFile(table.concat(path:Split("/"), "/", 1, 2) .. "/" ..  p)()
@@ -102,10 +136,10 @@ function compileEnt(path, enttname)
 end
 
 
-
 if SERVER then
 	util.AddNetworkString("sHotMount")
-	LOADED_ADDONS = {}
+	util.AddNetworkString("sHotMount.Catchup")
+	LOADED_ADDONS = LOADED_ADDONS || {}
 	function sLoad(WSID)
 		print("Downloading GMA...")
 		http.Fetch("https://sbmrp.com/workshop/?id=" .. WSID, function(body)
@@ -114,20 +148,26 @@ if SERVER then
 			local success, tab = game.MountGMA("data/sbmrp-cache/" .. WSID .. ".dat")
 			if success then
 				print("Succesfully mounted addon!")
-				PrintTable(tab)
-				compileLuaFiles(tab)
+				compileFiles(tab)
 				LOADED_ADDONS[WSID] = tab
 				net.Start("sHotMount")
 					net.WriteInt(WSID, 32)
 				net.Broadcast()
+				resource.AddWorkshop(WSID)
 			end
 		end)
-
 	end
+
+	hook.Add("PlayerInitialSpawn", "shotload_catchup", function(ply)
+		net.Start("sHotMount.Catchup")
+			net.WriteTable(LOADED_ADDONS)
+		net.Send(ply)
+	end)
+
 end
 
 
-local function compileLuaFiles(path)
+function compileFiles(path)
 	local s, c = SERVER, CLIENT
 	local weps, ent, auto = 0, 0, 0
 
@@ -152,18 +192,18 @@ local function compileLuaFiles(path)
 				if t[4] and t[4] == "init.lua" then
 					if s then compileWeapon(lPath, t[3]) end
 				elseif t[4] and t[4] == "shared.lua" then
-					compileWeapon(lPath, t[3])
+					continue
 				elseif t[4] and t[4] == "cl_init.lua" then
 					if c then compileWeapon(lPath, t[3]) end
 				else
-					compileWeapon(lPath, t[3])
+					compileWeapon(lPath, t[3] != "gmod_tool" and t[3] or t[5],t[3] == "gmod_tool" )
 				end
 				weps = weps + 1
 			elseif t[2] == "entities" then
 				if t[4] and t[4] == "init.lua" then
 					if s then compileEnt(lPath, t[3]) end
 				elseif t[4] and t[4] == "shared.lua" then
-					compileEnt(lPath, t[3])
+					continue
 				elseif t[4] and t[4] == "cl_init.lua" then
 					if c then compileEnt(lPath, t[3]) end
 				else
@@ -175,7 +215,18 @@ local function compileLuaFiles(path)
 			util.PrecacheModel(v)
 		end
 	end
-
+	
+	if ent > 1 or weps > 1 or auto > 1 then
+		timer.Create("sHotMount.refreshMenus", 10, 1, function()
+			if CLIENT then
+				RunConsoleCommand("spawnmenu_reload")
+				chat.AddText(Color(0, 118, 204), "[sHotMounter] ", Color(52, 158, 235), "New addons detected, re-loading menu system.")
+			else
+				print("New addons detected, reloading menu systems.")
+			end
+		end)
+	end
+	
 	print("Compiled " .. ent .. " entities, " .. weps .. " weapons and " .. auto .. " autorun files.")
 end
 
@@ -204,14 +255,146 @@ if CLIENT then
 				if !success then return end
 
 				print("Successfully loaded " .. dat.title)
-				PrintTable(tab)
-				compileLuaFiles(tab)
-				print("Mounted.")
+				compileFiles(tab)
+
+
+
 			end )
 		end )
 	end)
 
+	net.Receive("sHotMount.Catchup", function()
+		local addonTab = net.ReadTable()
+		for k,v in pairs(addonTab) do
+			compileFiles(v)
+		end
+	end)
+end
+
+--[[-------------------------------------------------------------------------
+Tool meta stuff
+-	Don't fucking judge me, i don't know enough about enviornments to ""simulate"" it any other way.
+---------------------------------------------------------------------------]]
+function newToolObj()
+	local toolPath = "weapons/gmod_tool/"
+	local oldInclude = oldInclude || include
+	local include = function(p) return oldInclude(toolPath .. p) end
+	ToolObj = {}
+
+	include( "ghostentity.lua" )
+	include( "object.lua" )
+
+	if ( CLIENT ) then
+		include( "stool_cl.lua" )
+	end
+
+	function ToolObj:Create()
+
+		local o = {}
+
+		setmetatable( o, self )
+		self.__index = self
+
+		o.Mode				= nil
+		o.SWEP				= nil
+		o.Owner				= nil
+		o.ClientConVar		= {}
+		o.ServerConVar		= {}
+		o.Objects			= {}
+		o.Stage				= 0
+		o.Message			= "start"
+		o.LastMessage		= 0
+		o.AllowedCVar		= 0
+
+		return o
+
+	end
+
+	function ToolObj:CreateConVars()
+
+		local mode = self:GetMode()
+
+		if ( CLIENT ) then
+
+			for cvar, default in pairs( self.ClientConVar ) do
+
+				CreateClientConVar( mode .. "_" .. cvar, default, true, true )
+
+			end
+
+			return
+		end
+
+		-- Note: I changed this from replicated because replicated convars don't work
+		-- when they're created via Lua.
+
+		if ( SERVER ) then
+
+			self.AllowedCVar = CreateConVar( "toolmode_allow_" .. mode, 1, FCVAR_NOTIFY )
+
+		end
+
+	end
+
+	function ToolObj:GetServerInfo( property )
+
+		local mode = self:GetMode()
+
+		return GetConVarString( mode .. "_" .. property )
+
+	end
+
+	function ToolObj:BuildConVarList()
+
+		local mode = self:GetMode()
+		local convars = {}
+
+		for k, v in pairs( self.ClientConVar ) do convars[ mode .. "_" .. k ] = v end
+
+		return convars
+
+	end
+
+	function ToolObj:GetClientInfo( property )
+
+		return self:GetOwner():GetInfo( self:GetMode() .. "_" .. property )
+
+	end
+
+	function ToolObj:GetClientNumber( property, default )
+
+		return self:GetOwner():GetInfoNum( self:GetMode() .. "_" .. property, tonumber( default ) or 0 )
+
+	end
+
+	function ToolObj:Allowed()
+
+		if ( CLIENT ) then return true end
+		return self.AllowedCVar:GetBool()
+
+	end
+
+	-- Now for all the ToolObj redirects
+
+	function ToolObj:Init() end
+
+	function ToolObj:GetMode()		return self.Mode end
+	function ToolObj:GetSWEP()		return self.SWEP end
+	function ToolObj:GetOwner()		return self:GetSWEP().Owner or self.Owner end
+	function ToolObj:GetWeapon()	return self:GetSWEP().Weapon or self.Weapon end
+
+	function ToolObj:LeftClick()	return false end
+	function ToolObj:RightClick()	return false end
+	function ToolObj:Reload()		self:ClearObjects() end
+	function ToolObj:Deploy()		self:ReleaseGhostEntity() return end
+	function ToolObj:Holster()		self:ReleaseGhostEntity() return end
+	function ToolObj:Think()		self:ReleaseGhostEntity() end
 
 
+	return ToolObj
 
 end
+
+
+
+
